0086d293a783c8a05d199e2569b273f7a51bdd78
mjsanchez@york.edu <mjsanchez@york.edu>
2024-01-22 20:36:07 -0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 === 0) {
+      console.log(`Even: ${i}`);
+    } else {
+      console.log(`Odd: ${i}`);
+    }
+  }
+
+ let _ = require('lodash');
+ 
+  let myResult = 'hora de comer';
+  
+console.log(_.camelCase(myResult));
+
