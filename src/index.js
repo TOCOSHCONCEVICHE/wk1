085546d1590a004d34b5df09d0d0585b2e6fd20a
mjsanchez@york.edu <mjsanchez@york.edu>
2024-01-28 17:41:01 -0600

    npm init

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
-for (let i = 1; i <= 100; i++) {
-    if (i % 2 === 0) {
-      console.log(`Even: ${i}`);
-    } else {
-      console.log(`Odd: ${i}`);
-    }
-  }
 
- let _ = require('lodash');
- 
-  let myResult = 'hora de comer';
-  
-console.log(_.camelCase(myResult));
+// Importing lodash functions
+const _ = require('lodash');
 
+// Example data
+const numbers = [1, 2, 3, 4, 5];
+const person = {
+    name: 'John Doe',
+    age: 30,
+    city: 'New York'
+};
+
+// Example 1: Using _.chunk()
+const chunkedNumbers = _.chunk(numbers, 2);
+console.log('Chunked numbers:', chunkedNumbers);
+
+// Example 2: Using _.isEmpty()
+console.log('Is person object empty?', _.isEmpty(person));
+
+// Example 3: Using _.sortBy()
+const sortedNumbers = _.sortBy(numbers);
+console.log('Sorted numbers:', sortedNumbers);
+
+// Example 4: Using _.map()
+const doubledNumbers = _.map(numbers, num => num * 2);
+console.log('Doubled numbers:', doubledNumbers);
+
+// Example 5: Using _.pick()
+const selectedFields = _.pick(person, ['name', 'age']);
+console.log('Selected fields:', selectedFields);
